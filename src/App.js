69fdda290ba079c26fc0fb739aea201ffa9c1aff
Lucas Jellema <lucasjellema@gmail.com>
2020-11-08 17:28:25 +0100

    Update App.js
    Adding custom content

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <h1>Greeting from a Static WebApp - running on Azure.</h1><div>Hello {value} and beyond</div>;
+  return <div><h1>Greeting from a Static WebApp - running on Azure.</h1>Hello {value} and beyond</div>;
 }
 
 export default App;
